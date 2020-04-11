b3a26257a589f8a9ab3905fc833b6b2cf902e257
Roman Zakrevskyi <romul3003@gmail.com>
2020-04-11 17:32:23 +0300

    Add input in Form component

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,11 +4,18 @@ class PostForm extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = {
+      title: '',
+    }
   }
 
   submitHandler = (event) => {
     event.preventDefault()
+  }
+
+  changeInputHandler = (event) => {
+    const { name, value } = event.target
+    this.setState((prevState) => ({ ...prevState, ...{ [name]: value } }))
   }
 
   render() {
@@ -16,7 +23,14 @@ class PostForm extends Component {
       <form onSubmit={this.submitHandler}>
         <div className="form-group">
           <label htmlFor="title">Заголовок поста</label>
-          <input type="text" className="form-control" id="title" />
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={this.state.title}
+            name="title"
+            onChange={this.changeInputHandler}
+          />
         </div>
         <button className="btn btn-success" type="submit">
           Создать
