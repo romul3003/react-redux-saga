ed51a7070352dc03774b1297eb791d013d039a66
Roman Zakrevskyi <romul3003@gmail.com>
2020-04-11 21:36:28 +0300

    Add Loader component

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -2,10 +2,16 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Post from './Post'
 import { fetchPosts } from '../redux/actions'
+import Loader from './Loader'
 
 const FetchedPosts = () => {
   const dispatch = useDispatch()
   const posts = useSelector((state) => state.posts.fetchedPosts)
+  const loading = useSelector((state) => state.app.loading)
+
+  if (loading) {
+    return <Loader />
+  }
 
   if (!posts.length) {
     return (
