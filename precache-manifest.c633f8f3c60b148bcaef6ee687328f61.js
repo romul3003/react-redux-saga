35d798082d5762996bc43f36439eb5f2bb59a5e6
Roman Zakrevskyi <romul3003@gmail.com>
2020-04-11 23:52:11 +0300

    Updates

---
+++ precache-manifest.c633f8f3c60b148bcaef6ee687328f61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718041270ed55dbe88e2c0a4c901bf8a",
+    "url": "/react-redux-saga/index.html"
+  },
+  {
+    "revision": "ceb49facf506be5f251a",
+    "url": "/react-redux-saga/static/js/2.8fce5f10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-saga/static/js/2.8fce5f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "333023abd700b3e98761",
+    "url": "/react-redux-saga/static/js/main.c86e8359.chunk.js"
+  },
+  {
+    "revision": "e7a6feaa2955641cbd7c",
+    "url": "/react-redux-saga/static/js/runtime-main.e63b2b80.js"
+  }
+]);
